824ee8c32bb87930d2253c1e0b5153b72cf1c02b
David <david.boeryd@gmail.com>
2018-06-02 17:41:00 +0200

    fixed infereior incorrect keys

--- keymap.c
+++ keymap.c
@@ -101,7 +101,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                   MO(SPEC),     KC_Y,    KC_U,    KC_I,    KC_O,              KC_P,             KC_LBRC,
                   KC_H,         KC_J,    KC_K,    KC_L,    LT(MDIA, KC_SCLN), GUI_T(KC_QUOT),
                   MEH_T(KC_NO), KC_N,    KC_M,    KC_COMM, KC_DOT,            CTL_T(KC_SLSH),   SFT_T(KC_BSLS),
-                  KC_UP,        KC_DOWN, KC_RALT, LSFT(KC_COMM), KC_FN1,
+                  KC_UP,        KC_DOWN, KC_RALT, KC_RBRC, KC_FN1,
 
                   KC_PGUP, KC_LALT,
                   KC_PGDN,
